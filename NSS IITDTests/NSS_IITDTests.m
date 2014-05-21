a6001da26c36f065518891bb641da3487dffc65e
Robin Malhotra <malhotra.robin@yahoo.co.in>
2014-05-21 21:59:43 +0530

    Initial Commit

---
+++ NSS IITDTests/NSS_IITDTests.m
@@ -0,0 +1,34 @@
+//
+//  NSS_IITDTests.m
+//  NSS IITDTests
+//
+//  Created by Robin Malhotra on 21/05/14.
+//  Copyright (c) 2014 Robin's code kitchen. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface NSS_IITDTests : XCTestCase
+
+@end
+
+@implementation NSS_IITDTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
